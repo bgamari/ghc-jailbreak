1fea6d14603d5c60e740ca959cda327be72c7e6e
Tamar Christina <tamar@zhox.com>
2019-04-28 18:35:30 +0100

    iat: Finished patching

--- fs.h
+++ fs.h
@@ -31,6 +31,10 @@ int FS(swopen) (const wchar_t* filename, int oflag,
                 int shflag, int pmode);
 FILE *FS(fwopen) (const wchar_t* filename, const wchar_t* mode);
 FILE *FS(fopen) (const char* filename, const char* mode);
+int FS(_stat) (const char *path, struct _stat *buffer);
+int FS(_stat64) (const char *path, struct __stat64 *buffer);
+int FS(_wstat) (const wchar_t *path, struct _stat *buffer);
+int FS(_wstat64) (const wchar_t *path, struct __stat64 *buffer);
 #else
 
 FILE *FS(fopen) (const char* filename, const char* mode);
