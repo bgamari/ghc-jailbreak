e5ab0ca7767639279e42c849cfb35db725703326
Ben Gamari <ben@smart-cactus.org>
2020-01-18 18:50:04 -0500

    Don't assume cwd

--- build-ghc.sh
+++ build-ghc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+TOP="$(readlink -f $(dirname $0))"
 bash_args="--login -c"
-make_args="make all -w -C `readlink -f $(dirname $0)` -f Makefile.ghc"
+make_args="make all -w -C $TOP -f Makefile.ghc"
 
 if [ -z "$MSYS32" ]; then MSYS32=/e/msys32; fi
 if [ -z "$MSYS64" ]; then MSYS64=/e/msys64; fi
@@ -11,4 +12,4 @@ set -x
 MSYSTEM=MING64 winpty $MSYS64/usr/bin/bash.exe $bash_args "PATH=/mingw64/bin:\$PATH $make_args"
 MSYSTEM=MING32 winpty $MSYS32/usr/bin/bash.exe $bash_args "PATH=/mingw32/bin:\$PATH $make_args"
 
-tar cvzf ghc-jailbreak.tar.gz i686/ x86_64/
+tar -C $TOP -cvzf ghc-jailbreak.tar.gz i686/ x86_64/
