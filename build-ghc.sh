d8eb4945f2e792092e79843e7ad9c063d42707c4
Ben Gamari <ben@smart-cactus.org>
2020-01-18 18:27:01 -0500

    build-ghc.sh: Remove environment dependence

--- build-ghc.sh
+++ build-ghc.sh
@@ -3,12 +3,12 @@
 bash_args="--login -c"
 make_args="make all -w -C `readlink -f .` -f Makefile.ghc"
 
-x86=/e/msys32/usr/bin/bash.exe
-x64=/e/msys64/usr/bin/bash.exe
+if [ -z "$MSYS32" ]; then MSYS32=/e/msys32; fi
+if [ -z "$MSYS64" ]; then MSYS64=/e/msys64; fi
 
 set -x
 
-MSYSTEM=MING64 winpty $x64 $bash_args "PATH=/mingw64/bin:\$PATH $make_args"
-MSYSTEM=MING32 winpty $x86 $bash_args "PATH=/mingw32/bin:\$PATH $make_args"
+MSYSTEM=MING64 winpty $MSYS64/usr/bin/bash.exe $bash_args "PATH=/mingw64/bin:\$PATH $make_args"
+MSYSTEM=MING32 winpty $MSYS32/usr/bin/bash.exe $bash_args "PATH=/mingw32/bin:\$PATH $make_args"
 
-tar cvzf ghc-jailbreak-0.5.tar.gz i686/ x86_64/
+tar cvzf ghc-jailbreak.tar.gz i686/ x86_64/
